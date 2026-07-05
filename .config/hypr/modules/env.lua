83d29c6f411eb612de0a8bbac96d9e3d916e9f38
gabrln <gabrln@users.noreply.github.com>
2026-07-05 14:38:03 -0300

    refactor: ajusta env.lua e Qt conforme recomendação do Noctalia
    - Remove QT_SCALE_FACTOR, GDK_SCALE, QT_STYLE_OVERRIDE e QT_QUICK_CONTROLS_STYLE do env.lua
    
    - Evita anulação do theming do Noctalia e permite escala automática HiDPI
    
    - Substitui qt6ct por qt6ct-kde para compatibilidade com apps KDE

--- .config/hypr/modules/env.lua
+++ .config/hypr/modules/env.lua
@@ -9,15 +9,11 @@ hl.env("XDG_SESSION_TYPE", "wayland")
 hl.env("GDK_BACKEND", "wayland,x11,*")
 hl.env("CLUTTER_BACKEND", "wayland")
 
--- Escala e temas para Qt/GTK
+-- Temas para Qt/GTK
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
 hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
 hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
-hl.env("QT_STYLE_OVERRIDE", "Fusion")
-hl.env("QT_QUICK_CONTROLS_STYLE", "Basic")
-hl.env("QT_SCALE_FACTOR", "1")
-hl.env("GDK_SCALE", "1")
 
 -- Aceleração para navegadores e Electron
 hl.env("MOZ_ENABLE_WAYLAND", "1")
